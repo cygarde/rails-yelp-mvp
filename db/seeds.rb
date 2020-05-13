b0a4facc9cc8010ac9c23a5bb52fb90e8e97b1f4
Cyril Garde <cygarde@free.fr>
2020-05-13 22:34:04 +0200

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cleaning database..."
+Restaurant.destroy_all
+
+puts "creating restaurants.."
+mamamia = { name: "mamamia", address:"100 rue roma", phone_number:"01020305", category:"italian"}
+miammiam = { name: "miammiam", address:"4 rue hugo", phone_number:"01020305", category:"french"}
+fritesss = { name: "fritesss", address:"2020 avenue franklin", phone_number:"01020305", category:"belgian"}
+itsnem = { name: "itsnem", address:"2 rue deserre", phone_number:"01020305", category:"chinese"}
+gingembre = { name: "gingembre", address:"centre ciale", phone_number:"01020305", category:"japanese"}
+
+[ mamamia, miammiam, fritesss, itsnem, gingembre  ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
